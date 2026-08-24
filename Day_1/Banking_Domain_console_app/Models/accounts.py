26484519813eba466e9c2fa21dd9ab2d8d44c5a9
Cam <150202195+CameronLewis08@users.noreply.github.com>
2026-08-24 11:18:31 -0400

    Initial setup of file hierarchy
    Created a directory for the banking application as well as a directory for the user, account, and bank models. Finally added a main.py file as our primary entry point.

---
+++ Day_1/Banking_Domain_console_app/Models/accounts.py
@@ -0,0 +1,7 @@
+"""
+Account Model for the Banking Domain Console Application:
+    This module defines the Account class, which represents an account in the banking domain. The Account class contains attributes such as account number, account type, and account balance. It also includes methods for managing account information and performing account operations.
+    This module should be used in conjunction with other modules in the banking domain to provide a complete banking application experience.
+    
+"""
+
---
+++ Day_1/Banking_Domain_console_app/Models/banks.py
@@ -0,0 +1,6 @@
+"""
+Bank Model for the Banking Domain Console Application:
+    This module defines the Bank class, which represents a bank in the banking domain. The Bank class contains attributes such as bank name, branch name, and a list of users associated with the bank. It also includes methods for managing users and performing banking operations.
+    This module should be used in conjunction with other modules in the banking domain to provide a complete banking application experience.
+    
+"""
